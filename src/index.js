c5e59f55a9c811899bbbc29b6a66f8a9b9b943d6
Elías Rodelo <elias.rodelo@talosdigital.com>
2018-04-03 11:18:54 -0500

    Sass loader added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style/index.css";
+import "./style/index.scss";
 import App from "./app/App";
 import registerServiceWorker from "./registerServiceWorker";
 
